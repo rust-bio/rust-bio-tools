f9b6e349198698ad159baf35ec35ecdfa8c54d6f
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-07-20 09:46:54 +0200

    Fixes.

--- src/bam/depth.rs
+++ src/bam/depth.rs
@@ -18,22 +18,45 @@ pub fn depth(bam_path: &str, max_read_length: u32) {
     let mut bam_reader = bam::IndexedReader::new(&bam_path).unwrap();
     let mut pos_reader = csv::Reader::from_reader(io::stdin()).has_headers(false).delimiter(b'\t');
     let mut csv_writer = csv::Writer::from_buffer(io::BufWriter::new(io::stdout())).delimiter(b'\t');
+    let mut last_tid = 0;
+    let mut last_pos = 0;
+    let mut exceeded = false;
+    let mut pileup_iter = bam_reader.pileup();
 
-    for record in pos_reader.decode() {
+    for (i, record) in pos_reader.decode().enumerate() {
         let record: PosRecord = record.unwrap();
+
+        // jump to correct position if necessary
         let tid = bam_reader.header.tid(record.chrom.as_bytes()).unwrap();
-        bam_reader.seek(tid, cmp::max(record.pos as i32 - max_read_length as i32, 0) as u32, record.pos).unwrap();
+        let start = cmp::max(record.pos as i32 - max_read_length as i32 - 1, 0) as u32;
+        if exceeded || tid != last_tid || start > last_pos || record.pos - 1 <= last_pos {
+            let n = bam_reader.header.target_len(tid).unwrap();
+            bam_reader.seek(tid, start, n).unwrap();
+            pileup_iter = bam_reader.pileup();
+        }
+
+        // iterate over pileups
         let mut covered = false;
-        for pileup in bam_reader.pileup() {
+        exceeded = false;
+        while let Some(pileup) = pileup_iter.next() {
             let pileup = pileup.unwrap();
-            covered = pileup.pos() == record.pos;
+            covered = pileup.pos() == record.pos - 1;
+            last_tid = pileup.tid();
+            last_pos = pileup.pos();
             if covered {
                 csv_writer.encode((&record.chrom, record.pos, pileup.depth())).unwrap();
+                break;
+            } else if pileup.pos() > record.pos {
+                exceeded = true;
                 break;
             }
         }
         if !covered {
             csv_writer.encode((&record.chrom, record.pos, 0)).unwrap();
+        }
+
+        if (i + 1) % 100 == 0 {
+            info!("{} records written.", i + 1);
         }
     }
 }

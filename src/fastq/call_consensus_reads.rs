deeba7ac28b48c2ab18ae7e5e0f76b6682c222b5
Henning Timm <henning.timm@tu-dortmund.de>
2018-11-21 16:13:42 +0100

    Removed deprecated function that is now part of cluster_consensus_reads

--- src/fastq/call_consensus_reads.rs
+++ src/fastq/call_consensus_reads.rs
@@ -22,47 +22,6 @@ use flate2::bufread::GzDecoder;
 const ALLELES: &'static [u8] = b"ACGT";
 
 
-// /// Collects UMIs from a reader on a p7 FASTQ file and returns them in a vector.
-// ///
-// /// This takes the first umi_len characters from each sequence in the file and
-// /// hence assumes that the UMI are the first characters in the line.
-// /// If the read other sequences before the UMI, these need to be trimmed before
-// /// this.
-// ///
-// /// The UMI sequences are cloned into the vector.
-// ///
-// /// # Errors
-// /// Passes on errors from bio::io::fastq::Reader i.e. fails if the file cannot
-// /// be opened.
-// ///
-// /// # Examples
-// ///
-// /// ```
-// /// let p7_fq = fastq::Reader::from_file(fq2);
-// /// let umi_len = 13;
-// /// let umis = umis(&mut p7_fq, umi_len)?;
-// /// ```
-// ///
-// fn umis<R: io::Read>(
-//     fq_reader: &mut fastq::Reader<R>,
-//     umi_len: usize,
-// ) -> Result<Vec<Vec<u8>>, Box<Error>> {
-//     let mut record = fastq::Record::new();
-//     let mut umis = Vec::new();
-//     loop {
-//         fq_reader.read(&mut record)?;
-//         if record.is_empty() {
-//             break;
-//         }
-
-//         let umi = record.seq()[..umi_len].to_owned();
-//         umis.push(umi);
-//     }
-
-//     Ok(umis)
-// }
-
-
 fn parse_cluster(record: csv::StringRecord) -> Result<Vec<usize>, Box<Error>> {
     let seqids = &record[2];
     Ok(csv::ReaderBuilder::new()

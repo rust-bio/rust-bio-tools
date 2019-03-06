3dce67ab0ab6ed8e31b4d98e846bc7abc4b4b3a0
Henning Timm <henning.timm@tu-dortmund.de>
2019-03-06 16:19:04 +0100

    Refactored error handling of main function.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use clap::App;
 use fern;
 use itertools::Itertools;
 use std::process;
+use std::error::Error;
 
 pub mod bam;
 pub mod bcf;
 pub mod fastq;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml)
         .version(env!("CARGO_PKG_VERSION"))
@@ -29,28 +30,22 @@ fn main() {
         .unwrap();
 
     if let Some(matches) = matches.subcommand_matches("fastq-split") {
-        if let Err(e) = fastq::split::split(&matches.values_of("chunks").unwrap().collect_vec()) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        fastq::split::split(&matches.values_of("chunks").unwrap().collect_vec())?;
+        Ok(())
     } else if let Some(matches) = matches.subcommand_matches("fastq-filter") {
-        if let Err(e) = fastq::filter::filter(&matches.value_of("ids").unwrap()) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        fastq::filter::filter(&matches.value_of("ids").unwrap())?;
+        Ok(())
     } else if let Some(matches) = matches.subcommand_matches("bam-depth") {
-        if let Err(e) = bam::depth::depth(
+        bam::depth::depth(
             &matches.value_of("bam-path").unwrap(),
             value_t!(matches, "max-read-length", u32).unwrap_or(1000),
             value_t!(matches, "include-flags", u16).unwrap_or(0),
             value_t!(matches, "exclude-flags", u16).unwrap_or(4 | 256 | 512 | 1024),
             value_t!(matches, "min-mapq", u8).unwrap_or(0),
-        ) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        )?;
+        Ok(())
     } else if let Some(matches) = matches.subcommand_matches("vcf-to-txt") {
-        if let Err(e) = bcf::to_txt::to_txt(
+        bcf::to_txt::to_txt(
             &matches
                 .values_of("info")
                 .map(|values| values.collect_vec())
@@ -60,26 +55,20 @@ fn main() {
                 .map(|values| values.collect_vec())
                 .unwrap_or(vec![]),
             matches.is_present("genotypes"),
-        ) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        )?;
+        Ok(())
     } else if let Some(matches) = matches.subcommand_matches("vcf-match") {
-        if let Err(e) = bcf::match_variants::match_variants(
+        bcf::match_variants::match_variants(
             matches.value_of("vcf").unwrap(),
             value_t!(matches, "max-dist", u32).unwrap_or(20),
             value_t!(matches, "max-len-diff", u32).unwrap_or(10),
-        ) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        )?;
+        Ok(())
     } else if let Some(_) = matches.subcommand_matches("vcf-baf") {
-        if let Err(e) = bcf::baf::calculate_baf() {
-            error!("{}", e);
-            process::exit(1);
-        }
+        bcf::baf::calculate_baf()?;
+        Ok(())
     } else if let Some(matches) = matches.subcommand_matches("call-consensus-reads") {
-        if let Err(e) = fastq::call_consensus_reads::call_consensus_reads_from_paths(
+        fastq::call_consensus_reads::call_consensus_reads_from_paths(
             matches.value_of("fq1").unwrap(),
             matches.value_of("fq2").unwrap(),
             matches.value_of("consensus-fq1").unwrap(),
@@ -87,9 +76,12 @@ fn main() {
             value_t!(matches, "umi-len", usize).unwrap(),
             value_t!(matches, "max-seq-dist", usize).unwrap(),
             value_t!(matches, "max-umi-dist", usize).unwrap(),
-        ) {
-            error!("{}", e);
-            process::exit(1);
-        }
+        )?;
+        Ok(())
+    } else {
+        // This cannot be reached, since the matches step of
+        // clap assures that a avalid subcommand is provided
+        Ok(())
     }
+    
 }

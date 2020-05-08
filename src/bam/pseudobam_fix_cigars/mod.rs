200f39db30c3073c93b64573cce07957db703e9e
David Laehnemann <david.laehnemann@hhu.de>
2020-05-08 18:03:39 +0200

    allow clipping in alignment, skip to around the record.pos for alignment

--- src/bam/pseudobam_fix_cigars/mod.rs
+++ src/bam/pseudobam_fix_cigars/mod.rs
@@ -27,21 +27,40 @@ pub fn pseudobam_fix_cigars(
         &bam::Header::from_template(&bam_reader.header()),
         bam::Format::BAM,
     )?;
+
     let mut bam_buffer = bam::buffer::RecordBuffer::new(bam_reader, false);
+
     // values as used by bwa, see: http://bio-bwa.sourceforge.net/bwa.shtml
-    let mut aligner = pairwise::Aligner::new(-6, -1, |a: u8, b: u8| if a == b {1i32} else {-4i32});
+    let scoring = pairwise::Scoring {
+        gap_open: -6,
+        gap_extend: -1,
+        match_fn: |a: u8, b: u8| if a == b {1i32} else {-4i32},
+        match_scores: Some((1, -4)),
+        xclip_prefix: -5,
+        xclip_suffix: -5,
+        yclip_prefix: pairwise::MIN_SCORE,
+        yclip_suffix: pairwise::MIN_SCORE
+    };
+    let mut aligner = pairwise::Aligner::with_scoring(scoring);
+    let check_upstream= 100;
+
     let mut reference_seq = Vec::new();
     for reference_name in bam_writer.header().target_names() {
-        println!("reference_name: {}", str::from_utf8(reference_name)? );
+//        println!("reference_name: {}", str::from_utf8(reference_name)? );
         if let Ok(_) = fasta_reader.fetch_all(str::from_utf8(reference_name)? ) {
             fasta_reader.read(&mut reference_seq)?;
             if let Ok((_, _)) = bam_buffer.fetch(reference_name, 0, reference_seq.len() as u32) {
                 for record in bam_buffer.iter_mut().collect_vec() {
-                    println!("reference_seq: {:?}", &reference_seq);
-                    println!("record sequence: {:?}", record.seq().as_bytes());
+//                    println!("reference_seq: {:?}", &reference_seq);
+//                    println!("record sequence: {:?}", record.seq().as_bytes());
                     let mut new_record = record.clone();
-                    let alignment = aligner.semiglobal( &record.seq().as_bytes().as_slice(), &reference_seq);
-                    println!("alignment: {:?}", alignment);
+                    let start_align: usize = if ( record.pos() as usize <= check_upstream ) {
+                        0
+                    } else {
+                        record.pos() as usize - check_upstream
+                    };
+                    let alignment = aligner.semiglobal( &record.seq().as_bytes().as_slice(), &reference_seq[start_align..]);
+//                    println!("alignment: {:?}", alignment);
                     // new function cigar_string_from_alignment_operations()
                     let cigar_string = CigarString(vec![]);
                     new_record.set(record.qname(), Some(&cigar_string), record.seq().encoded, record.qual());

f8092244ee68491d5092dd69edd895848967fcb3
David Laehnemann <david.laehnemann@hhu.de>
2020-05-14 20:26:14 +0200

    cache first read in pair to adjust fields once second in pair is processed

--- src/bam/pseudobam_fix_cigars/mod.rs
+++ src/bam/pseudobam_fix_cigars/mod.rs
@@ -3,12 +3,13 @@ use std::error::Error;
 use std::str;
 
 use rust_htslib::bam;
-use rust_htslib::bam::Read;
-use rust_htslib::bam::record::CigarString;
+use rust_htslib::bam::{Read, Record};
+use rust_htslib::bam::record::{Cigar, CigarString};
 
 use bio::io::fasta;
 use bio::alignment::{Alignment, pairwise};
 use itertools::Itertools;
+use std::collections::BTreeMap;
 
 pub fn pseudobam_fix_cigars(
     fasta_in: &str,
@@ -22,7 +23,7 @@ pub fn pseudobam_fix_cigars(
     let bam_reader = bam::IndexedReader::from_path(&bam_in)?;
 
     info!("Writing output bam with accurate correct CIGAR strings to:\n  {}", bam_out);
-    let bam_writer = bam::Writer::from_path(
+    let mut bam_writer = bam::Writer::from_path(
         bam_out,
         &bam::Header::from_template(&bam_reader.header()),
         bam::Format::BAM,
@@ -46,13 +47,11 @@ pub fn pseudobam_fix_cigars(
 
     let mut reference_seq = Vec::new();
     for reference_name in bam_writer.header().target_names() {
-//        println!("reference_name: {}", str::from_utf8(reference_name)? );
         if let Ok(_) = fasta_reader.fetch_all(str::from_utf8(reference_name)? ) {
+            let mut first_in_pairs: BTreeMap<&[u8], Record> = BTreeMap::new();
             fasta_reader.read(&mut reference_seq)?;
             if let Ok((_, _)) = bam_buffer.fetch(reference_name, 0, reference_seq.len() as u32) {
                 for record in bam_buffer.iter_mut().collect_vec() {
-//                    println!("reference_seq: {:?}", &reference_seq);
-//                    println!("record sequence: {:?}", record.seq().as_bytes());
                     let mut new_record = record.clone();
                     let start_align: usize = if ( record.pos() as usize <= check_upstream ) {
                         0
@@ -60,21 +59,36 @@ pub fn pseudobam_fix_cigars(
                         record.pos() as usize - check_upstream
                     };
                     let alignment = aligner.semiglobal( &record.seq().as_bytes().as_slice(), &reference_seq[start_align..]);
-//                    println!("alignment: {:?}", alignment);
                     // new function cigar_string_from_alignment_operations()
-                    let cigar_string = CigarString::from(alignment);
+                    let cigar_string = CigarString::from_alignment(&alignment, false);
                     new_record.set(record.qname(), Some(&cigar_string), record.seq().encoded, record.qual());
-                    //write out new record here
+                    new_record.set_pos( (aligner.ystart + start_align) as i32);
+                    if new_record.is_paired() {
+                        if new_record.is_first_in_template() {
+                            // need to wait for the second in the template to
+                            // include its position and insert size in the metadata
+                            first_in_pairs.insert(new_record.qname(), new_record);
+                        } else if new_record.is_last_in_template() {
+                            let mut first_in_pair = first_in_pairs.remove(new_record.qname())?;
+                            let insert_size = new_record.pos() - first_in_pair.pos() + ( (alignment.yend + start_align) as i32 - new_record.pos() );
+                            first_in_pair.set_mpos(new_record.pos());
+                            new_record.set_mpos(first_in_pair.pos());
+                            first_in_pair.set_insert_size(insert_size);
+                            new_record.set_insert_size(insert_size);
+                            bam_writer.write(&first_in_pair)?;
+                            bam_writer.write(&new_record)?;
+                        } else {
+                            panic!("Read is paired but neither first nor last in template.\n\
+                                    This should not happen in a pseudobam where only one mapping\n\
+                                    per transcript isoform is expected for each read in a pair.\n");
+                        }
+                    } else {
+                        bam_writer.write(&new_record)?;
+                    }
                 }
             }
         }
     }
 
     Ok(())
-}
-
-impl From<Alignment> for CigarString {
-    fn from(alignment: Alignment) -> Self {
-       
-    }
 }

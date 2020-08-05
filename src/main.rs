9fb9f8d3e32cbd7476314e66e3ede58f4d646ff0
Felix Mölder <felix.moelder@uni-due.de>
2020-08-05 12:16:59 +0200

    Add parameter for dgidb data sources

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             &matches.value_of("vcf").unwrap(),
             matches.value_of("api-path").unwrap().to_string(),
             &matches.value_of("field").unwrap(),
+            match matches.is_present("datasource") {
+                true => Some(matches.values_of("datasource").unwrap().collect()),
+                false => None
+            },
             value_t!(matches, "genes-per-request", usize).unwrap(),
         ),
         ("oncoprint", Some(matches)) => {

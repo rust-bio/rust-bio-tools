fb1513c3f0638fa166cb8550bfd0b000121427fa
Felix Wiegand <fxwiegand@wgdnet.de>
2020-10-06 10:20:12 +0200

    Create output path if it doesn't exist (#68)
    * create output path if it doesn't exist
    
    * create full path
    
    * revert change to create_dir_all and add error message
    
    Co-authored-by: Felix Mölder <felix.moelder@uni-due.de>

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             let mut sample_calls = HashMap::new();
             let mut bam_paths = HashMap::new();
             let output_path = matches.value_of("output-path").unwrap();
+            if !Path::new(output_path).exists() {
+                fs::create_dir(Path::new(output_path)).unwrap_or_else(|_| {
+                    panic!(
+                        "Couldn't create output directory at {}. Please make sure the path exists.",
+                        output_path
+                    )
+                });
+            }
             let max_cells = u32::from_str(matches.value_of("max-cells").unwrap()).unwrap();
             let custom_js = matches.value_of("custom-js");
             bcf::report::embed_js(output_path, custom_js)?;

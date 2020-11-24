3eb51fc0022221d30391093197f3650f933d2135
Felix Mölder <felix.moelder@uni-due.de>
2020-11-24 16:57:13 +0100

    Just a few dbgs

--- src/bam/collapse_reads_to_fragments/calc_consensus.rs
+++ src/bam/collapse_reads_to_fragments/calc_consensus.rs
@@ -139,7 +139,19 @@ impl<'a> CalcOverlappingConsensus<'a> {
             StrandObservation::Forward => consensus_strand.push(b'+'),
             StrandObservation::Reverse => consensus_strand.push(b'-'),
             StrandObservation::Both => consensus_strand.push(b'*'),
-            StrandObservation::None => unreachable!(),
+            StrandObservation::None => {
+                dbg!(&self.recs1()[0].len());
+                dbg!(&self.recs1()[0].pos());
+                dbg!(&self.recs1()[0].cigar_cached().unwrap().end_pos());
+                dbg!(&self.recs1()[0].cigar_cached().unwrap());
+                dbg!(&self.recs2()[0].len());
+                dbg!(&self.recs2()[0].pos());
+                dbg!(&self.recs2()[0].cigar_cached().unwrap());
+                dbg!(&base_pos);
+                dbg!(&first_end_pos);
+                dbg!(&self.overlap());
+                dbg!(&second_start_pos);
+                unreachable!()},
         }
     }
 }

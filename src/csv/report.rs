d1f60b379b2ef4a729f3d58a3f98c32ec45527bd
Felix Wiegand <fxwiegand@wgdnet.de>
2021-03-18 18:50:18 +0100

    Update src/csv/report.rs
    Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>

--- src/csv/report.rs
+++ src/csv/report.rs
@@ -443,7 +443,7 @@ fn nominal_plot(table: &[HashMap<String, String>], column: String) -> Option<Vec
     }
 
     if plot_data.len() > 10 {
-        let unique_values = count_values.iter().map(|(_, v)| v).collect::<HashSet<_>>();
+        let unique_values: HashSet<_> = count_values.iter().map(|(_, v)| v).collect();
         if unique_values.len() <= 1 {
             return None;
         };

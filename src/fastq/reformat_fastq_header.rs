e2a4b264d0ecfda83e1e2703167f746a5894f03e
Felix Mölder <felix.moelder@uni-due.de>
2022-09-29 14:03:08 +0200

    replace desc

--- src/fastq/reformat_fastq_header.rs
+++ src/fastq/reformat_fastq_header.rs
@@ -28,26 +28,24 @@ fn process_fastq(desc_regex: &Regex, desc_format: &str, capture_groups: &[String
         let reg = Handlebars::new();
         let record = result?;
         let description_opt = record.desc();
-        let description_extended = match description_opt {
+        let description_updated = match description_opt {
             Some(description) => {
-                let mut description_extended = description.to_owned();
+                let mut description_updated = String::new();
                 for caps in desc_regex.captures_iter(description) {
                     let mut field_replacements = HashMap::new();
                     for field in capture_groups {
                         field_replacements.insert(field, caps.name(field).unwrap().as_str());
                     }
                     let rendered_entry = reg.render_template(desc_format, &field_replacements)?;
-                    //TODO That's dirty
-                    description_extended.push(' ');
-                    description_extended.push_str(&rendered_entry);
+                    description_updated = format!("{} {}", description_updated, rendered_entry);
                 }
-                Some(description_extended)
+                Some(description_updated)
             }
             None => None,
         };
         let record_out = fastq::Record::with_attrs(
             record.id(),
-            description_extended.as_deref(),
+            description_updated.as_deref(),
             record.seq(),
             record.qual(),
         );
